9bac62081232b57438183c468e1d4c95d7372472
aminul <rahathssn@gmail.com>
2025-05-01 15:05:45 +0600

    added why us section

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,9 +1,13 @@
 import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
+import Specialist from "@/components/UI/HomePage/Specialist/Specialist";
+import WhyUs from "@/components/UI/HomePage/WhyUs/WhyUs";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
+      <Specialist />
+      <WhyUs />
     </>
   );
 };
